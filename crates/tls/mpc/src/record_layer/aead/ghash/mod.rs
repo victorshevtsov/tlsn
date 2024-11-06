41aeee845618e1568eb8101e8cb7aa95c42e6eff
th4s <th4s@metavoid.xyz>
2024-11-06 16:54:03 +0100

    Added tag module.

--- crates/tls/mpc/src/record_layer/aead/ghash/mod.rs
+++ crates/tls/mpc/src/record_layer/aead/ghash/mod.rs
@@ -1,14 +1,16 @@
 //! This module implements [`UniversalHash`](super::UniversalHash) for Ghash.
 
-use crate::{MpcTlsError, TlsRole};
+use crate::{decode::OneTimePadShared, MpcTlsError, TlsRole};
 use mpz_common::Context;
 use mpz_core::{
     commit::{Decommitment, HashCommit},
     hash::Hash,
 };
+use mpz_fields::gf2_128::Gf2_128;
+use mpz_share_conversion::{AdditiveToMultiplicative, MultiplicativeToAdditive, ShareConvert};
 use serde::{Deserialize, Serialize};
 use serio::{stream::IoStreamExt, SinkExt};
-use std::ops::Add;
+use std::{future::Future, ops::Add};
 use tracing::instrument;
 
 mod error;
@@ -21,9 +23,114 @@ pub(crate) use ghash_inner::{Ghash, GhashConfig, GhashConfigBuilder, GhashConfig
 pub(crate) struct Tag(Vec<u8>);
 
 impl Tag {
-    /// Creates a new tag.
-    pub(crate) fn new(bytes: Vec<u8>) -> Self {
-        Self(bytes)
+    /// Computes the tag for a ciphertext and additional data.
+    ///
+    /// The commit-reveal step is not required for computing a tag sent to the
+    /// server, as it will be able to detect if the tag is incorrect.
+    ///
+    /// # Arguments
+    ///
+    pub(crate) async fn compute<Ctx, C, J, Sc>(
+        ctx: &mut Ctx,
+        ghash: &mut Ghash<Sc>,
+        j0: OneTimePadShared,
+        ciphertext: C,
+        aad: Vec<u8>,
+    ) -> Result<Self, MpcTlsError>
+    where
+        Ctx: Context,
+        C: Future<Output = Result<Vec<u8>, MpcTlsError>>,
+        Sc: ShareConvert<Gf2_128>,
+        Sc: AdditiveToMultiplicative<Gf2_128, Future: Send>,
+        Sc: MultiplicativeToAdditive<Gf2_128, Future: Send>,
+    {
+        let j0 = j0.decode().await?;
+        let ciphertext = ciphertext.await?;
+
+        let ciphertext_padded = build_ghash_data(aad, ciphertext.clone());
+        let hash = ghash.finalize(ciphertext_padded)?;
+
+        let tag_share = j0
+            .into_iter()
+            .zip(hash.into_iter())
+            .map(|(a, b)| a ^ b)
+            .collect();
+        let tag_share = Tag(tag_share);
+
+        // TODO: The follower doesn't really need to learn the tag,
+        // we could reduce some latency by not sending it.
+        let io = ctx.io_mut();
+
+        io.send(tag_share.clone()).await?;
+        let other_tag_share: Tag = io.expect_next().await?;
+
+        let tag = tag_share + other_tag_share;
+        Ok(tag)
+    }
+
+    /// Verifies a purported tag against`self`.
+    ///
+    /// Verifying a tag requires a commit-reveal protocol between the leader and
+    /// follower. Without it, the party which receives the other's tag share first
+    /// could trivially compute a tag share which would cause an invalid message to
+    /// be accepted.
+    ///
+    /// # Arguments
+    ///
+    /// * `ctx`           - The context for IO.
+    /// * `role`          - The role of the party.
+    /// * `purported_tag` - The tag to verify against `self`.
+    #[instrument(level = "debug", skip_all, err)]
+    pub(crate) async fn verify<Ctx: Context>(
+        self,
+        ctx: &mut Ctx,
+        role: TlsRole,
+        purported_tag: Vec<u8>,
+    ) -> Result<(), MpcTlsError> {
+        let io = ctx.io_mut();
+        let tag = match role {
+            TlsRole::Leader => {
+                // Send commitment of tag share to follower.
+                let (decommitment, commitment) = self.clone().hash_commit();
+
+                io.send(commitment).await?;
+
+                let follower_share: Tag = io.expect_next().await?;
+
+                // Send decommitment (tag share) to follower.
+                io.send(decommitment).await?;
+
+                self + follower_share
+            }
+            TlsRole::Follower => {
+                // Wait for commitment from leader.
+                let commitment: Hash = io.expect_next().await?;
+
+                // Send tag share to leader.
+                io.send(self.clone()).await?;
+
+                // Expect decommitment (tag share) from leader.
+                let decommitment: Decommitment<Tag> = io.expect_next().await?;
+
+                // Verify decommitment.
+                decommitment.verify(&commitment).map_err(|_| {
+                    MpcTlsError::peer("leader tag share commitment verification failed")
+                })?;
+
+                let leader_share = decommitment.into_inner();
+
+                self + leader_share
+            }
+        };
+
+        let purported_tag = Tag(purported_tag);
+
+        // Reject if tag is incorrect.
+        if tag != purported_tag {
+            return Err(MpcTlsError::tag("invalid tag"));
+        }
+
+        Ok(())
     }
 
     /// Returns the underlying bytes.
@@ -42,98 +149,13 @@ impl Add for Tag {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        let tag = self
-            .0
-            .into_iter()
-            .zip(rhs.0.into_iter())
-            .map(|(a, b)| a ^ b)
-            .collect();
-
+        let tag = self.0.into_iter().zip(rhs.0).map(|(a, b)| a ^ b).collect();
         Self(tag)
     }
 }
 
-/// Computes the tag for a ciphertext and additional data.
-///
-/// The commit-reveal step is not required for computing a tag sent to the
-/// Server, as it will be able to detect if the tag is incorrect.
-#[instrument(level = "debug", skip_all, err)]
-pub(crate) async fn add_tag_shares<Ctx: Context>(
-    ctx: &mut Ctx,
-    share: Tag,
-) -> Result<Tag, MpcTlsError> {
-    // TODO: The follower doesn't really need to learn the tag,
-    // we could reduce some latency by not sending it.
-    let io = ctx.io_mut();
-
-    io.send(share.clone()).await?;
-    let other_tag_share: Tag = io.expect_next().await?;
-
-    let tag = share + other_tag_share;
-
-    Ok(tag)
-}
-
-/// Verifies a purported tag against the ciphertext and additional data.
-///
-/// Verifying a tag requires a commit-reveal protocol between the leader and
-/// follower. Without it, the party which receives the other's tag share first
-/// could trivially compute a tag share which would cause an invalid message to
-/// be accepted.
-#[instrument(level = "debug", skip_all, err)]
-#[allow(clippy::too_many_arguments)]
-pub(crate) async fn verify_tag<Ctx: Context>(
-    ctx: &mut Ctx,
-    role: TlsRole,
-    share: Tag,
-    purported_tag: Tag,
-) -> Result<(), MpcTlsError> {
-    let io = ctx.io_mut();
-    let tag = match role {
-        TlsRole::Leader => {
-            // Send commitment of tag share to follower.
-            let (decommitment, commitment) = share.clone().hash_commit();
-
-            io.send(commitment).await?;
-
-            let follower_share: Tag = io.expect_next().await?;
-
-            // Send decommitment (tag share) to follower.
-            io.send(decommitment).await?;
-
-            share + follower_share
-        }
-        TlsRole::Follower => {
-            // Wait for commitment from leader.
-            let commitment: Hash = io.expect_next().await?;
-
-            // Send tag share to leader.
-            io.send(share.clone()).await?;
-
-            // Expect decommitment (tag share) from leader.
-            let decommitment: Decommitment<Tag> = io.expect_next().await?;
-
-            // Verify decommitment.
-            decommitment.verify(&commitment).map_err(|_| {
-                MpcTlsError::peer("leader tag share commitment verification failed")
-            })?;
-
-            let leader_share = decommitment.into_inner();
-
-            share + leader_share
-        }
-    };
-
-    // Reject if tag is incorrect.
-    if tag.into_inner() != purported_tag.into_inner() {
-        return Err(MpcTlsError::tag("invalid tag"));
-    }
-
-    Ok(())
-}
-
 /// Builds padded data for GHASH.
-pub(crate) fn build_ghash_data(mut aad: Vec<u8>, mut ciphertext: Vec<u8>) -> Vec<u8> {
+fn build_ghash_data(mut aad: Vec<u8>, mut ciphertext: Vec<u8>) -> Vec<u8> {
     let associated_data_bitlen = (aad.len() as u64) * 8;
     let text_bitlen = (ciphertext.len() as u64) * 8;
 
--- crates/tls/mpc/src/record_layer/aead/mod.rs
+++ crates/tls/mpc/src/record_layer/aead/mod.rs
@@ -1,13 +1,196 @@
 //! Handles authenticated encryption, decryption and tags.
 
+use crate::{
+    decode::{Decode, OneTimePadShared},
+    MpcTlsError, TlsRole,
+};
+use cipher::{CipherCircuit, Keystream};
+use mpz_common::Context;
+use mpz_core::bitvec::BitVec;
+use mpz_fields::gf2_128::Gf2_128;
 use mpz_memory_core::{
     binary::{Binary, U8},
-    FromRaw, Slice, StaticSize, ToRaw, Vector,
+    MemoryExt, Repr, StaticSize, Vector, View, ViewExt,
 };
+use mpz_memory_core::{DecodeFutureTyped, Memory};
+use mpz_memory_core::{FromRaw, Slice, ToRaw};
+use mpz_share_conversion::{AdditiveToMultiplicative, MultiplicativeToAdditive, ShareConvert};
+use mpz_vm_core::Vm;
+use tracing::instrument;
 
-pub(crate) mod decrypt;
-pub(crate) mod encrypt;
 pub(crate) mod ghash;
+use ghash::{Ghash, GhashConfig, GhashConfigBuilder, GhashConfigBuilderError, Tag};
+
+#[instrument(level = "trace", skip_all, err)]
+pub(crate) fn prepare_tag_for_encrypt<V, C>(
+    vm: &mut V,
+    role: TlsRole,
+    j0: <C as CipherCircuit>::Block,
+    ciphertext: Vector<U8>,
+    aad: Vec<u8>,
+) -> Result<TagCreator, MpcTlsError>
+where
+    V: Vm<Binary> + Memory<Binary> + View<Binary>,
+    C: CipherCircuit,
+{
+    let j0: Vector<U8> = transmute(j0);
+    let j0 = Decode::new(vm, role, j0)?;
+    let j0 = j0.shared(vm)?;
+
+    let ciphertext = vm.decode(ciphertext).map_err(MpcTlsError::vm)?;
+
+    let text = TagCreator {
+        j0,
+        ciphertext,
+        aad,
+    };
+
+    Ok(text)
+}
+
+pub(crate) struct TagCreator {
+    j0: OneTimePadShared,
+    ciphertext: DecodeFutureTyped<BitVec<u32>, Vec<u8>>,
+    aad: Vec<u8>,
+}
+
+impl TagCreator {
+    #[instrument(level = "trace", skip_all, err)]
+    pub(crate) async fn compute<Ctx, U, Sc>(
+        self,
+        ghash: &mut Ghash<Sc>,
+        ctx: &mut Ctx,
+    ) -> Result<Vec<u8>, MpcTlsError>
+    where
+        Ctx: Context,
+        Sc: ShareConvert<Gf2_128>,
+        Sc: AdditiveToMultiplicative<Gf2_128, Future: Send>,
+        Sc: MultiplicativeToAdditive<Gf2_128, Future: Send>,
+    {
+        let j0 = self.j0.decode().await?;
+        let aad = self.aad;
+
+        let mut ciphertext = self.ciphertext.await?;
+
+        let ciphertext_padded = build_ghash_data(aad, ciphertext.clone());
+        let hash = ghash.finalize(ciphertext_padded)?;
+
+        let tag_share = j0
+            .into_iter()
+            .zip(hash.into_iter())
+            .map(|(a, b)| a ^ b)
+            .collect();
+        let tag_share = Tag::new(tag_share);
+
+        let tag = add_tag_shares(ctx, tag_share).await?;
+        ciphertext.extend(&tag.into_inner());
+
+        Ok(ciphertext)
+    }
+}
+
+#[instrument(level = "trace", skip_all, err)]
+pub(crate) fn decrypt<V, C>(
+    vm: &mut V,
+    role: TlsRole,
+    keystream: &mut Keystream<C>,
+    explicit_nonce: <<C as CipherCircuit>::Nonce as Repr<Binary>>::Clear,
+    start_counter: u32,
+    mut ciphertext: Vec<u8>,
+    aad: Vec<u8>,
+) -> Result<PlainText, MpcTlsError>
+where
+    V: Vm<Binary> + View<Binary>,
+    C: CipherCircuit,
+    <<C as CipherCircuit>::Counter as Repr<Binary>>::Clear: From<[u8; 4]>,
+    <<C as CipherCircuit>::Nonce as Repr<Binary>>::Clear: Copy,
+{
+    let tag_bytes = ciphertext
+        .split_off(ciphertext.len() - <<C as CipherCircuit>::Block as StaticSize<Binary>>::SIZE);
+    let purported_tag = Tag::new(tag_bytes);
+
+    let len = ciphertext.len();
+    let block_size = <<C as CipherCircuit>::Block as StaticSize<Binary>>::SIZE / 8;
+    let block_count = (len / block_size) + (len % block_size != 0) as usize;
+
+    let j0 = keystream
+        .j0(vm, explicit_nonce)
+        .map_err(MpcTlsError::decrypt)?;
+    let j0: Vector<U8> = transmute(j0);
+    let j0 = Decode::new(vm, role, j0)?;
+    let j0 = j0.shared(vm)?;
+
+    let keystream = keystream.chunk(block_count).map_err(MpcTlsError::decrypt)?;
+    let cipher_ref = vm.alloc_vec(len).map_err(MpcTlsError::vm)?;
+    vm.mark_public(cipher_ref).map_err(MpcTlsError::vm)?;
+
+    let cipher_output = keystream
+        .apply(vm, cipher_ref)
+        .map_err(MpcTlsError::decrypt)?;
+
+    let plaintext = cipher_output
+        .assign(vm, explicit_nonce, start_counter, ciphertext.clone())
+        .map_err(MpcTlsError::decrypt)?;
+
+    let plaintext = PlainText {
+        role,
+        j0,
+        ciphertext,
+        purported_tag,
+        plaintext,
+        aad,
+    };
+
+    Ok(plaintext)
+}
+
+pub(crate) struct PlainText {
+    role: TlsRole,
+    j0: OneTimePadShared,
+    ciphertext: Vec<u8>,
+    purported_tag: Tag,
+    plaintext: Vector<U8>,
+    aad: Vec<u8>,
+}
+
+impl PlainText {
+    #[instrument(level = "trace", skip_all, err)]
+    pub(crate) async fn compute<Ctx, Sc>(
+        self,
+        ghash: &mut Ghash<Sc>,
+        ctx: &mut Ctx,
+    ) -> Result<Vector<U8>, MpcTlsError>
+    where
+        Ctx: Context,
+        Sc: ShareConvert<Gf2_128>,
+        Sc: AdditiveToMultiplicative<Gf2_128, Future: Send>,
+        Sc: MultiplicativeToAdditive<Gf2_128, Future: Send>,
+    {
+        let PlainText {
+            role,
+            j0,
+            ciphertext,
+            purported_tag,
+            plaintext,
+            aad,
+        } = self;
+
+        let j0 = j0.decode().await?;
+        let ciphertext = build_ghash_data(aad, ciphertext);
+        let hash = ghash.finalize(ciphertext)?;
+
+        let tag_share = j0
+            .into_iter()
+            .zip(hash.into_iter())
+            .map(|(a, b)| a ^ b)
+            .collect();
+        let tag_share = Tag::new(tag_share);
+
+        verify_tag(ctx, role, tag_share, purported_tag).await?;
+
+        Ok(plaintext)
+    }
+}
 
 fn transmute<T>(value: T) -> Vector<U8>
 where
