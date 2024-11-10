b859dada07fa2d2d886b16481b08a8c3f683458e
th4s <th4s@metavoid.xyz>
2024-11-10 16:13:56 +0100

    Rewrite leader dependency injection.

--- crates/tls/mpc/src/components.rs
+++ crates/tls/mpc/src/components.rs
@@ -1,61 +1,45 @@
+use crate::{
+    record_layer::aead::ghash::{Ghash, GhashConfig},
+    TlsRole,
+};
+use cipher::aes::MpcAes;
 use hmac_sha256::{MpcPrf, Prf, PrfConfig, Role as PrfRole};
 use key_exchange::{KeyExchange, KeyExchangeConfig, MpcKeyExchange, Role as KeRole};
-use mpz_common::{Context, Preprocess};
 use mpz_fields::{gf2_128::Gf2_128, p256::P256};
-use mpz_ole::rot::{OLEReceiver, OLESender};
-use mpz_ot::{OTError, RandomOTReceiver, RandomOTSender};
-use mpz_share_conversion::{ShareConversionReceiver, ShareConversionSender};
-use tlsn_universal_hash::{
-    ghash::{Ghash, GhashConfig},
-    UniversalHash,
-};
+use mpz_memory_core::{binary::Binary, Memory, View};
+use mpz_ole::{ROLEReceiver, ROLESender};
+use mpz_share_conversion::{ShareConversionReceiver, ShareConversionSender, ShareConvert};
+use mpz_vm_core::Vm;
 
-use crate::{MpcTlsCommonConfig, TlsRole};
-
-/// Builds the components for MPC-TLS.
-pub fn build_components<K, P, C, U, OTS, OTR>(
-    role: TlsRole,
-    config: &MpcTlsCommonConfig,
-    ctx: Ctx,
-    vm: V,
-    ot_send: OTS,
-    ot_recv: OTR,
-) -> (K, P, C, U, C, U)
+/// Builds the components for MPC-TLS leader.
+pub fn build_leader<V, RSP, RRP, RSGF>(
+    rs_p: RSP,
+    rr_p: RRP,
+    rs_gf0: RSGF,
+    rs_gf1: RSGF,
+) -> (
+    impl KeyExchange<V>,
+    impl Prf<V>,
+    MpcAes,
+    Ghash<impl ShareConvert<Gf2_128>>,
+    Ghash<impl ShareConvert<Gf2_128>>,
+)
 where
-    Ctx: Context + 'static,
-    OTS: Preprocess<Ctx, Error = OTError>
-        + RandomOTSender<Ctx, [P256; 2]>
-        + RandomOTSender<Ctx, [Gf2_128; 2]>
-        + Clone
-        + Send
-        + Sync
-        + 'static,
-    OTR: Preprocess<Ctx, Error = OTError>
-        + RandomOTReceiver<Ctx, bool, P256>
-        + RandomOTReceiver<Ctx, bool, Gf2_128>
-        + Clone
-        + Send
-        + Sync
-        + 'static,
+    V: Vm<Binary> + View<Binary> + Memory<Binary> + Send,
+    RSP: ROLESender<P256> + Send,
+    RRP: ROLEReceiver<P256> + Send,
+    RSGF: ROLESender<Gf2_128> + Send,
 {
-    let ke = match role {
-        TlsRole::Leader => MpcKeyExchange::new(
-            KeyExchangeConfig::builder()
-                .role(KeRole::Leader)
-                .build()
-                .unwrap(),
-            ShareConversionSender::new(OLESender::new(ot_send.clone())),
-            ShareConversionReceiver::new(OLEReceiver::new(ot_recv.clone())),
-        ),
-        TlsRole::Follower => MpcKeyExchange::new(
-            KeyExchangeConfig::builder()
-                .role(KeRole::Follower)
-                .build()
-                .unwrap(),
-            ShareConversionReceiver::new(OLEReceiver::new(ot_recv.clone())),
-            ShareConversionSender::new(OLESender::new(ot_send.clone())),
-        ),
-    };
+    let role = TlsRole::Leader;
+
+    let ke = MpcKeyExchange::new(
+        KeyExchangeConfig::builder()
+            .role(KeRole::Leader)
+            .build()
+            .unwrap(),
+        ShareConversionSender::new(rs_p),
+        ShareConversionReceiver::new(rr_p),
+    );
 
     let prf = MpcPrf::new(
         PrfConfig::builder()
@@ -67,27 +51,70 @@ where
             .unwrap(),
     );
 
-    let ghash_encrypt = match role {
-        TlsRole::Leader => Ghash::new(
-            GhashConfig::builder().build().unwrap(),
-            ShareConversionSender::new(OLESender::new(ot_send.clone())),
-        ),
-        TlsRole::Follower => Ghash::new(
-            GhashConfig::builder().build().unwrap(),
-            ShareConversionReceiver::new(OLEReceiver::new(ot_recv.clone())),
-        ),
-    };
+    let cipher = MpcAes::default();
 
-    let ghash_decrypt = match role {
-        TlsRole::Leader => Ghash::new(
-            GhashConfig::builder().build().unwrap(),
-            ShareConversionSender::new(OLESender::new(ot_send)),
-        ),
-        TlsRole::Follower => Ghash::new(
-            GhashConfig::builder().build().unwrap(),
-            ShareConversionReceiver::new(OLEReceiver::new(ot_recv)),
-        ),
-    };
+    let ghash_encrypt = Ghash::new(
+        GhashConfig::builder().build().unwrap(),
+        ShareConversionSender::new(rs_gf0),
+    );
+    let ghash_decrypt = Ghash::new(
+        GhashConfig::builder().build().unwrap(),
+        ShareConversionSender::new(rs_gf1),
+    );
 
-    (ke, prf, ghash_encrypt, ghash_decrypt)
+    (ke, prf, cipher, ghash_encrypt, ghash_decrypt)
+}
+
+/// Builds the components for MPC-TLS follower.
+pub fn build_follower<V, RSP, RRP, RRGF>(
+    rs_p: RSP,
+    rr_p: RRP,
+    rr_gf0: RRGF,
+    rr_gf1: RRGF,
+) -> (
+    impl KeyExchange<V>,
+    impl Prf<V>,
+    MpcAes,
+    Ghash<impl ShareConvert<Gf2_128>>,
+    Ghash<impl ShareConvert<Gf2_128>>,
+)
+where
+    V: Vm<Binary> + View<Binary> + Memory<Binary> + Send,
+    RSP: ROLESender<P256> + Send,
+    RRP: ROLEReceiver<P256> + Send,
+    RRGF: ROLEReceiver<Gf2_128> + Send,
+{
+    let role = TlsRole::Follower;
+
+    let ke = MpcKeyExchange::new(
+        KeyExchangeConfig::builder()
+            .role(KeRole::Follower)
+            .build()
+            .unwrap(),
+        ShareConversionReceiver::new(rr_p),
+        ShareConversionSender::new(rs_p),
+    );
+
+    let prf = MpcPrf::new(
+        PrfConfig::builder()
+            .role(match role {
+                TlsRole::Leader => PrfRole::Leader,
+                TlsRole::Follower => PrfRole::Follower,
+            })
+            .build()
+            .unwrap(),
+    );
+
+    let cipher = MpcAes::default();
+
+    let ghash_encrypt = Ghash::new(
+        GhashConfig::builder().build().unwrap(),
+        ShareConversionReceiver::new(rr_gf0),
+    );
+    let ghash_decrypt = Ghash::new(
+        GhashConfig::builder().build().unwrap(),
+        ShareConversionReceiver::new(rr_gf1),
+    );
+
+    (ke, prf, cipher, ghash_encrypt, ghash_decrypt)
 }
